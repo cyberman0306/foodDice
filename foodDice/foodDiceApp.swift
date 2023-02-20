eabc651fcfe00475cc462eaacb22486cfc1a75f2
yook <yhj03069@gmail.com>
2023-02-20 21:25:46 +0900

    Initial Commit

---
+++ foodDice/foodDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodDiceApp.swift
+//  foodDice
+//
+//  Created by yook on 2023/02/20.
+//
+
+import SwiftUI
+
+@main
+struct foodDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
